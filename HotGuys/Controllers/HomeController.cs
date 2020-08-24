6271cee627bfb517762ebf301d21572a56fc9c2f
Lee <flii0013@student.monash.edu>
2020-08-24 22:25:48 +0800

    add cart functionality

--- HotGuys/Controllers/HomeController.cs
+++ HotGuys/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using HotGuys.Utils;
 
 namespace HotGuys.Controllers
 {
@@ -27,6 +28,13 @@ namespace HotGuys.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             HotChoiceViewModels hotChoiceViewModels = db.HotChoiceViewModels.Include(h => h.User).FirstOrDefault(h => h.Id == id);
+            if (Session["count"] == null)
+            {
+                Session["count"] = "0";
+            }
+            int count = Session["count"].ToString().Length;
+            Session["temp"] = "No."+count + "Name:" + hotChoiceViewModels.Name + "   Price:" + hotChoiceViewModels.Price.ToString();
+            Session["front"] = "Details";
             if (hotChoiceViewModels == null)
             {
                 return HttpNotFound();
@@ -41,6 +49,29 @@ namespace HotGuys.Controllers
             // ViewData["Comments"] =  db.Comments.Where(c => c.HotChoiceId == hotChoiceViewModels.Id).ToList();
             return View(hotChoiceViewModels);
         }
+        public ActionResult Cart()
+        {
+            if (Session["cart"] == null)
+            {
+                Session["cart"] = "";
+            }
+            if (Session["temp"] == null)
+            {
+                Session["temp"] = "";
+            }
+            if (Session["count"] == null)
+            {
+                Session["count"] = "0";
+            }
+            if (Session["front"].ToString() == "Details")
+            {
+                Session["cart"] += Session["temp"].ToString();
+                Session["count"] += "0";
+            }
+            ViewBag.Message = "Your shopping cart list:" +Session["cart"].ToString();
+
+            return View();
+        }
         public ActionResult Store()
         {
             var hotchoices = db.HotChoiceViewModels.Include(h => h.User).ToList();
@@ -54,6 +85,7 @@ namespace HotGuys.Controllers
                 }
  
             }
+            Session["front"] = "Store";
             return View(hotchoices);
             
         }
@@ -92,6 +124,38 @@ namespace HotGuys.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Please contact us at support@hotguys.com";
+
+            return View();
+        }
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents);
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
 
             return View();
         }
